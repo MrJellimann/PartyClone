c5d2f1e10340e1b9b6041752110466ebdb1f2316
Dootuz <95551048+Dootuz@users.noreply.github.com>
2023-04-28 21:06:11 -0400

    art
    added snowman art andballs

--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
     [SerializeField]
-    private int score;
+    private int score = 0;
+
+    public Text scoreText; // Reference to the UI text object to display the score
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateScoreText(); // Update the score text when the game starts
+    }
 
     public void AddScore(int value)
     {
         score += value;
+        UpdateScoreText(); // Update the score text when the score changes
     }
 
     public void SubScore(int value)
@@ -18,10 +28,31 @@ public class PlayerScore : MonoBehaviour
             score = 0;
         else
             score -= value;
+        UpdateScoreText(); // Update the score text when the score changes
     }
 
     public void SetScore(int value)
     {
         score = value;
+        UpdateScoreText(); // Update the score text when the score changes
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BadBall"))
+        {
+            SubScore(5);
+            Destroy(other.gameObject); // Destroy the BadBall object
+        }
+        else if (other.gameObject.CompareTag("GoodBall"))
+        {
+            AddScore(10);
+            Destroy(other.gameObject); // Destroy the GoodBall object
+        }
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString(); // Update the text of the score text object
     }
 }
